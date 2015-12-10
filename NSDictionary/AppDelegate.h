3c23e4aba6c38ef0bd720f28c24a50879fdfd744
dungdang236 <dungdang4593@gmail.com>
2015-12-10 08:57:14 -0800

    Initial Commit

---
+++ NSDictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDictionary
+//
+//  Created by student on 10/12/2015.
+//  Copyright © 2015 dungdang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
